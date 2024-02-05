91865c4b0644b7259f6ed63b63b6073539339754
ilikali <ialliik147@gmail.com>
2024-02-05 13:04:57 +0400

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { cookies, headers } from "next/headers";
 import Link from "next/link";
 
 async function getData() {
+  const cookiesConcent = cookies();
+  const headersContent = headers();
   const response = await fetch(
-    `https://api.bakoo.az/v2/ru/filter?time=${Date.now()}`
+    `https://api.bakoo.az/v2/ru/az/folders?brand=bmw&model=x5&page=1&per_page=20&time=${Date.now()}`
   );
   return {
     info: await response.json(),
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,4 +1,8 @@
+import { cookies, headers } from "next/headers";
+
 async function getData() {
+  const cookiesConcent = cookies();
+  const headersContent = headers();
   const response = await fetch(
     `https://api.bakoo.az/v2/ru/filter?time=${Date.now()}`
   );
